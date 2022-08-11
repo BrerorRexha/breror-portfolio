8d9ebdcad4faa7fe099cb812c2fb144fe9233bb4
Breror Rexha <breror@gjirafa.com>
2022-08-11 17:49:14 +0200

    added navigation

--- scripts/index.js
+++ scripts/index.js
@@ -19,5 +19,12 @@ function introAnimation() {
     document.querySelectorAll('.text-container').forEach((textContainer) => {
       textContainer.classList.add('hidden');
     });
+    document.querySelector('.front').classList.add('main-display');
+    document.querySelector('.back').classList.add('main-display');
   }, 5500);
+  setTimeout(() => {
+    document.querySelectorAll('.item').forEach((item) => {
+      item.classList.add('slide');
+    });
+  }, 6500);
 }
