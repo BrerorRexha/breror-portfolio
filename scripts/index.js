23e6b10a3c42a8395f9be38b68581e246886bc2f
Breror Rexha <breror@gjirafa.com>
2022-08-11 17:10:19 +0200

    intro animation update

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,11 @@ window.onload = () => {
 };
 
 function introAnimation() {
-  document.querySelectorAll('.intro-text').forEach((introText) => {
-    introText.classList.add('slide');
+  document.querySelectorAll('.text-container').forEach((textContainer) => {
+    textContainer.classList.add('slide');
   });
   document.querySelector('.front').classList.add('slice');
+  document.querySelectorAll('.intro-text').forEach((introText) => {
+    introText.classList.add('slide-up');
+  });
 }
