df3dda3b4a9da1a79feea795cff296f32300376e
Breror Rexha <breror@gjirafa.com>
2022-08-11 17:18:47 +0200

    finished intro animation

--- scripts/index.js
+++ scripts/index.js
@@ -10,4 +10,14 @@ function introAnimation() {
   document.querySelectorAll('.intro-text').forEach((introText) => {
     introText.classList.add('slide-up');
   });
+  setTimeout(() => {
+    document.querySelectorAll('.text-container').forEach((textContainer) => {
+      textContainer.classList.remove('slide');
+    });
+  }, 4500);
+  setTimeout(() => {
+    document.querySelectorAll('.text-container').forEach((textContainer) => {
+      textContainer.classList.add('hidden');
+    });
+  }, 5500);
 }
